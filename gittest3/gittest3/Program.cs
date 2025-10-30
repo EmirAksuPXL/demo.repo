24d236cd45a71a547c7f26b3cb47ea093445c246
Emir Aksu <Emir.Aksu@student.pxl.be>
2025-10-30 16:18:57 +0100

    test4

--- gittest3/gittest3/Program.cs
+++ gittest3/gittest3/Program.cs
@@ -7,6 +7,10 @@
             Console.WriteLine("Emir2");
             Console.WriteLine("test2");
             Console.WriteLine("test3");
+
+
+
+            Console.WriteLine("test4");
         }
     }
 }
